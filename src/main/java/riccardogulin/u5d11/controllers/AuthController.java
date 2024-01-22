637b5338e48768fe10212e18e2ca898cb5e8e0aa
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-01-22 13:11:41 +0100

    Moves users registration endpoint to /auth/register

--- src/main/java/riccardogulin/u5d11/controllers/AuthController.java
+++ src/main/java/riccardogulin/u5d11/controllers/AuthController.java
@@ -1,23 +1,45 @@
 package riccardogulin.u5d11.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import riccardogulin.u5d11.entities.User;
+import riccardogulin.u5d11.exceptions.BadRequestException;
+import riccardogulin.u5d11.payloads.users.NewUserDTO;
+import riccardogulin.u5d11.payloads.users.NewUserResponseDTO;
 import riccardogulin.u5d11.payloads.users.UserLoginDTO;
 import riccardogulin.u5d11.payloads.users.UserLoginResponseDTO;
 import riccardogulin.u5d11.services.AuthService;
+import riccardogulin.u5d11.services.UsersService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 	@Autowired
 	AuthService authService;
+	@Autowired
+	UsersService usersService;
 
 	@PostMapping("/login")
 	public UserLoginResponseDTO login(@RequestBody UserLoginDTO body) {
 		String accessToken = authService.authenticateUser(body);
 		return new UserLoginResponseDTO(accessToken);
+	}
+
+	@PostMapping("/register")
+	@ResponseStatus(HttpStatus.CREATED)
+	public NewUserResponseDTO createUser(@RequestBody @Validated NewUserDTO newUserPayload, BindingResult validation) {
+		// Per completare la validazione devo in qualche maniera fare un controllo del tipo: se ci sono errori -> manda risposta con 400 Bad Request
+		System.out.println(validation);
+		if (validation.hasErrors()) {
+			System.out.println(validation.getAllErrors());
+			throw new BadRequestException("Ci sono errori nel payload!"); // L'eccezione arriverà agli error handlers tra i quali c'è quello che manderà la risposta con status code 400
+		} else {
+			User newUser = usersService.save(newUserPayload);
+
+			return new NewUserResponseDTO(newUser.getId());
+		}
 	}
 }
--- src/main/java/riccardogulin/u5d11/controllers/UsersController.java
+++ src/main/java/riccardogulin/u5d11/controllers/UsersController.java
@@ -3,13 +3,8 @@ package riccardogulin.u5d11.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import riccardogulin.u5d11.entities.User;
-import riccardogulin.u5d11.exceptions.BadRequestException;
-import riccardogulin.u5d11.payloads.users.NewUserDTO;
-import riccardogulin.u5d11.payloads.users.NewUserResponseDTO;
 import riccardogulin.u5d11.services.UsersService;
 
 import java.util.UUID;
@@ -32,20 +27,6 @@ public class UsersController {
 		return usersService.findById(userId);
 	}
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public NewUserResponseDTO createUser(@RequestBody @Validated NewUserDTO newUserPayload, BindingResult validation) {
-		// Per completare la validazione devo in qualche maniera fare un controllo del tipo: se ci sono errori -> manda risposta con 400 Bad Request
-		System.out.println(validation);
-		if (validation.hasErrors()) {
-			System.out.println(validation.getAllErrors());
-			throw new BadRequestException("Ci sono errori nel payload!"); // L'eccezione arriverà agli error handlers tra i quali c'è quello che manderà la risposta con status code 400
-		} else {
-			User newUser = usersService.save(newUserPayload);
-
-			return new NewUserResponseDTO(newUser.getId());
-		}
-	}
 
 	@PutMapping("/{userId}")
 	public User getUserByIdAndUpdate(@PathVariable UUID userId, @RequestBody User modifiedUserPayload) {
